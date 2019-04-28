34d1c03b383932ec8b5ef1207f7491b9c2e9ad98
Jawad Khawaja <devjawkha@gmail.com>
2019-04-28 23:38:12 +0500

    dna pairing

---
+++ 23-pair-element.js
@@ -0,0 +1,40 @@
+/**
+ * Intermediate Algorithm Scripting: DNA Pairing
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+Return the provided character as the first element in each array.
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+ */
+
+function pairElement(str) {
+  let paired = [];
+
+  function withPair(letter) {
+    switch (letter) {
+      case 'A':
+        return ['A', 'T'];
+
+      case 'T':
+        return ['T', 'A'];
+
+      case 'G':
+        return ['G', 'C'];
+
+      case 'C':
+        return ['C', 'G'];
+
+      default:
+        break;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    paired.push(withPair(str[i]));
+  }
+  return paired;
+}
+
+pairElement('ATCGA'); // [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]
+pairElement('TTGAG'); // [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]]
+pairElement('CTCTA'); // [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]
